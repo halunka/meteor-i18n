fd48b71c92bb0926fd591e661124474488b746a1
Kriegslustig <github@ls7.ch>
2015-07-28 18:45:15 +0200

    Add a basic package.js file

---
+++ Package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  name: 'halunka:i18n',
+  version: '0.0.0',
+  summary: 'Lightweight, isomorphic, extendable i18n package for meteor using MongoDB',
+  git: 'https://github.com/halunka/meteor-i18n'
+})
+
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@1.1')
+  api.use('mongo')
+})
