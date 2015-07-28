9d6bb907d75cef50305c51c4aa8e46f341b72467
Kriegslustig <github@ls7.ch>
2015-07-28 21:37:03 +0200

    Further clean up the API

--- i18n.js
+++ i18n.js
@@ -5,13 +5,26 @@ i18n.state = new ReactiveDict('i18nValues')
 i18n.trns = new ReactiveDict('i18nTrns')
 i18n.acts = {}
 
-i18n.default = function i18nDefault (newValue) {
-  return this.state.set('default', newValue)
-}
-
 i18n.add = function i18nAdd (defTrns, trnsObj) {
   trnsObj[this.state.get('default')] = defTrns
   return this.db.upsert(trnsObj, { $set: trnsObj })
+}
+
+i18n.get = function i18nget (key, lang) {
+  lang = typeof lang == 'string' ? lang : this.get()
+  var query = {}
+  var act
+  var trns
+  query[this.state.get('default')] = key
+  act = this.db.findOne.bind(this.db, query)
+  this.acts[key + lang] = act
+  this.trns.set(key + lang, act())
+  trns = this.trns.get(key + lang)
+  return (trns && trns[lang])
+}
+
+i18n.defaultLang = function i18nDefault (newValue) {
+  return this.state.set('default', newValue)
 }
 
 i18n.addLang = function i18nAddLang (key, str) {
@@ -28,26 +41,13 @@ i18n.getLang = function i18nGet () {
   return this.state.get('currLang') || this.state.get('default') || 'en'
 }
 
-i18n.list = function i18nList () {
+i18n.listLang = function i18nList () {
   return _.map(this.state.get('langs'), function (val, key) {
     return {
       key: key,
       name: val
     }
   })
-}
-
-i18n.get = function i18nget (key, lang) {
-  lang = typeof lang == 'string' ? lang : this.get()
-  var query = {}
-  var act
-  var trns
-  query[this.state.get('default')] = key
-  act = this.db.findOne.bind(this.db, query)
-  this.acts[key + lang] = act
-  this.trns.set(key + lang, act())
-  trns = this.trns.get(key + lang)
-  return (trns && trns[lang])
 }
 
 i18n.updateTrns = function i18nUpdateTrns () {
