8ab5649eee8eb63043b63bc3e86bd440ba5449e1
Kriegslustig <github@ls7.ch>
2015-07-29 17:55:52 +0200

    Fix a few bugs

--- i18n.js
+++ i18n.js
@@ -3,29 +3,30 @@ i18n = {}
 i18n.db = new Mongo.Collection('halunka:i18n')
 i18n.state = new ReactiveDict('i18nValues')
 i18n._trns = new ReactiveDict('i18nTrns')
-i18n._acts = {}
+i18n.state.set('langs', {})
 
 i18n.add = function i18nAdd (defTrns, trnsObj) {
-  _.extend(trnsObj, i18n.state.get('langs'))
-  trnsObj[i18n.state.get('default')] = defTrns
+  trnsObj = _.extend(i18n.state.get('langs'), trnsObj)
+  trnsObj[i18n.getDefaultLang()] = defTrns
   return i18n.db.upsert(trnsObj, { $set: trnsObj })
 }
 
 i18n.get = function i18nget (key, lang) {
-  lang = typeof lang == 'string' ? lang : i18n.get()
-  var query = {}
-  var act
-  var trns
-  query[i18n.state.get('default')] = key
-  act = i18n.db.findOne.bind(i18n.db, query)
-  i18n._acts[key + lang] = act
-  i18n._trns.set(key + lang, act())
-  trns = i18n._trns.get(key + lang)
-  return (trns && trns[lang])
+  lang = typeof lang == 'string' ? lang : i18n.getLang()
+  console.log(i18n.db.find().fetch())
+  console.log([i18n._trns.get(key), key])
+  return maybeGet(i18n._trns.get(key), lang)
 }
 
-i18n.defaultLang = function i18nDefault (newValue) {
-  return i18n.state.set('default', newValue)
+i18n.defaultLang = function i18nDefaultLang (newValue) {
+  i18n.state.set('default', newValue)
+  i18n.updateTrns()
+  return newValue
+}
+
+i18n.getDefaultLang = function i18nGetDefaultLang (newValue) {
+  return i18n.state.get('default') ||
+    _.keys(i18n.state.get('langs'))[0]
 }
 
 i18n.addLang = function i18nAddLang (key, str) {
@@ -40,8 +41,7 @@ i18n.setLang = function i18nSet (lang) {
 
 i18n.getLang = function i18nGet () {
   return i18n.state.get('currLang') ||
-    i18n.state.get('default') ||
-    _.keys(i18n.state.get('langs'))[0]
+    i18n.getDefaultLang()
 }
 
 i18n.listLang = function i18nList () {
@@ -54,11 +54,17 @@ i18n.listLang = function i18nList () {
 }
 
 i18n.updateTrns = function i18nUpdateTrns () {
-  var self = i18n
-  _.each(i18n._acts, function (act, key) {
-    self._trns.set(key, act())
+  i18n.db.find().forEach(function (doc) {
+    var def = i18n.getDefaultLang()
+    i18n._trns.set(doc[def], _.omit(doc, '_id'))
   })
 }
+
+function maybeGet (obj, key) {
+  return obj ? obj[key] : ''
+}
+
+i18n.updateTrns()
 
 if(Meteor.isServer) {
   Meteor.publish('AllTranslations', function () {
