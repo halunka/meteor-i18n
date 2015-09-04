ea6a590776ab646c48f45650894a1e26094c5921
Kriegslustig <github@ls7.ch>
2015-09-04 18:43:23 +0200

    Clean up the API

--- i18n.js
+++ i18n.js
@@ -7,42 +7,42 @@ i18n.state.set('langs', {})
 
 i18n.add = function i18nAdd (defTrns, trnsObj) {
   trnsObj = _.extend(i18n.state.get('langs'), trnsObj)
-  trnsObj[i18n.getDefaultLang()] = defTrns
+  trnsObj[i18n.getDefaultLanguage()] = defTrns
   return i18n.db.upsert(trnsObj, { $set: trnsObj })
 }
 
 i18n.get = function i18nget (key, lang) {
-  lang = typeof lang == 'string' ? lang : i18n.getLang()
+  lang = typeof lang == 'string' ? lang : i18n.getLanguage()
   return maybeGet(i18n._trns.get(key), lang)
 }
 
-i18n.defaultLang = function i18nDefaultLang (newValue) {
+i18n.defaultLanguage = function i18nDefaultLang (newValue) {
   i18n.state.set('default', newValue)
-  i18n.updateTrns()
+  i18n.updateTranslations()
   return newValue
 }
 
-i18n.getDefaultLang = function i18nGetDefaultLang (newValue) {
+i18n.getDefaultLanguage = function i18nGetDefaultLanguage (newValue) {
   return i18n.state.get('default') ||
     _.keys(i18n.state.get('langs'))[0]
 }
 
-i18n.addLang = function i18nAddLang (key, str) {
+i18n.addLanguage = function i18nAddLanguage (key, str) {
   var langs = i18n.state.get('langs') || {}
   langs[key] = str
   i18n.state.set('langs', langs)
 }
 
-i18n.setLang = function i18nSet (lang) {
+i18n.setLanguage = function i18nSet (lang) {
   return i18n.state.set('currLang', lang)
 }
 
-i18n.getLang = function i18nGet () {
+i18n.getLanguage = function i18nGet () {
   return i18n.state.get('currLang') ||
-    i18n.getDefaultLang()
+    i18n.getDefaultLanguage()
 }
 
-i18n.listLang = function i18nList () {
+i18n.listLanguages = function i18nListLanguages () {
   return _.map(i18n.state.get('langs'), function (val, key) {
     return {
       key: key,
@@ -51,9 +51,9 @@ i18n.listLang = function i18nList () {
   })
 }
 
-i18n.updateTrns = function i18nUpdateTrns () {
+i18n.updateTranslations = function i18nUpdateTrns () {
   i18n.db.find().forEach(function (doc) {
-    var def = i18n.getDefaultLang()
+    var def = i18n.getDefaultLanguage()
     i18n._trns.set(doc[def], _.omit(doc, '_id'))
   })
 }
@@ -62,7 +62,7 @@ function maybeGet (obj, key) {
   return obj ? obj[key] : ''
 }
 
-i18n.updateTrns()
+i18n.updateTranslations()
 
 if(Meteor.isServer) {
   Meteor.publish('AllTranslations', function () {
@@ -72,7 +72,7 @@ if(Meteor.isServer) {
 
 if(Meteor.isClient) {
   i18n.sub = Meteor.subscribe('AllTranslations', function () {
-    i18n.updateTrns()
+    i18n.updateTranslations()
   })
   Template.registerHelper('i18nget', i18n.get)
   Template.registerHelper('i18nlist', i18n.listLang)
--- tests/i18n.js
+++ tests/i18n.js
@@ -1,49 +1,52 @@
-function clearState () {
+function clearState (cb) {
   i18n.state.keys = {};
+  i18n.db.remove({}, cb())
 }
 
 Tinytest.add('i18n', function (test) {
   test.equal(typeof i18n, 'object', 'Export an object i18n to both client and server')
 })
 
-Tinytest.add('i18n.addLang', function (test) {
-  i18n.addLang('en', 'English')
+Tinytest.addAsync('i18n.addLanguage', function (test, done) {
+  i18n.addLanguage('en', 'English')
   test.equal(i18n.state.get('langs')['en'], 'English', 'Add a key-value pair to i18n.state.langs')
-  clearState()
+  clearState(done)
 })
 
-Tinytest.add('i18n.setLang', function (test) {
-  i18n.setLang('en')
+Tinytest.addAsync('i18n.setLanguage', function (test, done) {
+  i18n.setLanguage('en')
   test.equal(i18n.state.get('currLang'), 'en', 'Set the i18n.state.currLang to the passed value')
-  clearState()
+  clearState(done)
 })
 
-Tinytest.add('i18n.defaultLang', function (test) {
-  i18n.addLang('en', 'English')
-  i18n.defaultLang('en')
+Tinytest.addAsync('i18n.defaultLanguage', function (test, done) {
+  i18n.addLanguage('en', 'English')
+  i18n.defaultLanguage('en')
   test.equal(i18n.state.get('default'), 'en', 'Should set state.default')
-  clearState()
+  clearState(done)
 })
 
-Tinytest.add('i18n.getLang', function (test) {
-  i18n.addLang('en', 'English')
-  test.equal(i18n.getLang(), 'en', 'Return the first value if no default and no currLang is set')
-  i18n.defaultLang('ru')
-  test.equal(i18n.getLang(), 'ru', 'Return the default if no currLang is set')
-  i18n.setLang('de')
-  test.equal(i18n.getLang(), 'de', 'Return the currLang if it\'s set')
-  clearState()
+Tinytest.addAsync('i18n.getLanguage', function (test, done) {
+  i18n.addLanguage('en', 'English')
+  test.equal(i18n.getLanguage(), 'en', 'Return the first value if no default and no currLang is set')
+  i18n.defaultLanguage('ru')
+  test.equal(i18n.getLanguage(), 'ru', 'Return the default if no currLang is set')
+  i18n.setLanguage('de')
+  test.equal(i18n.getLanguage(), 'de', 'Return the currLang if it\'s set')
+  clearState(done)
 })
 
-Tinytest.add('i18n.listLang', function (test) {
-  i18n.addLang('en', 'English')
-  test.equal(i18n.listLang().length, 1, 'Return an array of all the registered languages')
+Tinytest.addAsync('i18n.listLanguages', function (test, done) {
+  i18n.addLanguage('en', 'English')
+  i18n.addLanguage('de', 'Other')
+  test.equal(i18n.listLanguages().length, 2, 'Return an array of all the registered languages')
+  clearState(done)
 })
 
 if(Meteor.isServer) {
-  Tinytest.add('i18n.add', function (test) {
-    i18n.defaultLang('en')
-    i18n.addLang('ru', 'Russian')
+  Tinytest.addAsync('i18n.add', function (test, done) {
+    i18n.defaultLanguage('en')
+    i18n.addLanguage('ru', 'Russian')
     i18n.add('test.i18n.add', {
       de: 'test'
     })
@@ -52,22 +55,24 @@ if(Meteor.isServer) {
     i18n.add('test.i18n.add', {})
     test.equal(i18n.db.find({en: 'test.i18n.add'}).count(), 1, 'Use upsert')
     test.equal(i18n.db.findOne({en: 'test.i18n.add'})['de'], 'test', 'Insert the object passed')
+    clearState(done)
   })
 }
 
-Tinytest.add('i18n.get', function (test) {
+Tinytest.addAsync('i18n.get', function (test, done) {
   if(Meteor.isServer) {
-    i18n.addLang('en', 'English')
-    i18n.addLang('de', 'Deutsch')
-    i18n.addLang('rg', 'Rumantsch')
-    i18n.defaultLang('en')
+    i18n.addLanguage('en', 'English')
+    i18n.addLanguage('de', 'Deutsch')
+    i18n.addLanguage('rg', 'Rumantsch')
+    i18n.defaultLanguage('en')
     i18n.add('test.i18n.geten', {
       de: 'test.i18n.getde',
       rg: 'test.i18n.getrg'
     })
-    i18n.setLang('de')
+    i18n.setLanguage('de')
     test.equal(i18n.get('test.i18n.geten'), 'test.i18n.getde', 'Should return the string in the current lang')
-    i18n.setLang('rg')
+    i18n.setLanguage('rg')
     test.equal(i18n.get('test.i18n.geten'), 'test.i18n.getrg', 'Should be able to handle more that two languages')
+    clearState(done)
   }
 })
