ec838a121a2a1a10a3663a1352f5ab93ae059de2
Kriegslustig <github@ls7.ch>
2015-09-06 20:14:26 +0200

    Pass the tests for the new API

--- i18n.js
+++ i18n.js
@@ -6,50 +6,24 @@ i18n.state.set('langs', {})
 i18n.dep = new Tracker.Dependency
 i18n.depChanged = i18n.dep.changed.bind(i18n.dep)
 
-i18n.add = function i18nAdd (dataArr, cb) {
-  var done = cb ?
-    _.compose(cb, i18n.depChanged):
-    i18n.depChanged
-  dataArr = [].concat(dataArr)
-  for(var i = 0; i < dataArr.length - 1; i++) {
-    i18n.insert(dataArr[i])
-  }
-  i18n.insert(dataArr[i], done)
-  return dataArr
-}
-
-i18n.insert = function i18nInsert (translations, done) {
-  var oldId
-  var lang = i18n.getDefaultLanguage()
-  oldId = ( i18n.db.findOne(constructObject(lang, translations[lang])) || {} )._id
-  translations = _.extend(i18n.state.get('langs'), translations)
-  if(oldId) return i18n.db.update(oldId, { $set: translations }, done)
-  return i18n.db.insert(translations, done)
-}
+i18n.db.find().observe({
+  addded: i18n.depChanged,
+  changed: i18n.depChanged,
+  removed: i18n.depChanged
+})
 
 i18n.get = function i18nget (key, lang) {
   lang = typeof lang == 'string' ? lang : i18n.getLanguage()
-  return maybeGet(i18n.reactiveQuery(key), lang)
+  return maybeGet(i18n.reactiveQuery(queryWithKey(key)), lang)
 }
 
 i18n.getAll = function i18nGetAll (key) {
-  lang = typeof lang == 'string' ? lang : i18n.getLanguage()
-  return _.omit(i18n.reactiveQuery(key), '_id')
+  return i18n.reactiveQuery(queryWithKey(key))
 }
 
-i18n.reactiveQuery = function i18nReactiveQuery (key) {
+i18n.reactiveQuery = function i18nReactiveQuery (query) {
   i18n.dep.depend()
-  return i18n.db.findOne(constructObject(i18n.getDefaultLanguage(), key))
-}
-
-i18n.setDefaultLanguage = function i18nSetDefaultLang (newValue) {
-  i18n.state.set('default', newValue)
-  return newValue
-}
-
-i18n.getDefaultLanguage = function i18nGetDefaultLanguage () {
-  return i18n.state.get('default') ||
-    _.keys(i18n.state.get('langs') || [])[0]
+  return _.values(_.omit(i18n.db.findOne(query), '_id'))[0]
 }
 
 i18n.addLanguage = function i18nAddLanguage (key, str) {
@@ -63,8 +37,7 @@ i18n.setLanguage = function i18nSet (lang) {
 }
 
 i18n.getLanguage = function i18nGet () {
-  return i18n.state.get('currLang') ||
-    i18n.getDefaultLanguage()
+  return i18n.state.get('currLang')
 }
 
 i18n.listLanguages = function i18nListLanguages () {
@@ -80,34 +53,79 @@ function maybeGet (obj, key) {
   return obj ? obj[key] : ''
 }
 
-function constructObject (key, value) {
-  var obj = {}
+function genObject (key, value, obj) {
+  obj = obj || {}
   obj[key] = value
   return obj
 }
 
+function queryWithKey (key) {
+  return genObject(escKey(key), {$exists: true})
+}
+
+function flattenObj (obj, last, parent) {
+  return _.reduce(
+    obj,
+    function (memo, value, key) {
+      var newKey = parent ? [parent, key].join('.') : key
+      if (typeof value === 'object') {
+        _.extend(memo, flattenObj(value, last, newKey))
+      } else {
+        if (last) {
+          memo[parent] ? memo[parent][key] = value : memo[parent] = genObject(key, value)
+        } else {
+          memo[newKey] = value
+        }
+      }
+      return memo
+    },
+  {})
+}
+
+function escKey (key) {
+  return key.split('.').join('-')
+}
+
+function escKeysObj (obj) {
+  return _.object(_.map(_.keys(obj), escKey), _.values(obj))
+}
+
 if(Meteor.isServer) {
+
+  i18n.add = function i18nAdd (data, lang, parent) {
+    data = flattenObj(data, !lang)
+    data = escKeysObj(data)
+    _.each(data, insert.bind(null, lang))
+  }
+
+  function insert (lang, translation, key) {
+    return i18n.db.upsert(
+      ( i18n.db.findOne(queryWithKey(key)) || {} )._id,
+      { $set: genObject(lang ? [key, lang].join('.') : key, translation) }
+    )
+  }
 
   Meteor.publish('i18n:all', function () {
     return i18n.db.find()
   })
+
   Meteor.publish('i18n:specific', function (lang) {
-    return i18n.db.find({ fields: [lang] })
+    return i18n.db.find({fields: lang})
   })
 
 } else {
   Template.registerHelper('i18nget', i18n.get)
   Template.registerHelper('i18nlist', i18n.listLanguage)
 
-  i18n.loadSpecific = function i18nLoadSpecific (lang, cb) {
-    Meteor.subscribe('i18n:specific', lang, function () {
+  i18n.loadAll = function i18nLoadAll (cb) {
+    Meteor.subscribe('i18n:all', function () {
       i18n.depChanged()
       if(cb) cb()
     })
   }
 
-  i18n.loadAll = function i18nLoadAll (cb) {
-    Meteor.subscribe('i18n:all', function () {
+  i18n.loadSpecific = function i18nLoadSpecific (lang, cb) {
+    Meteor.subscribe('i18n:specific', lang, function () {
       i18n.depChanged()
       if(cb) cb()
     })
