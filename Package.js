5558d060691ecb90f89a7504c65ad89cf49fff8b
Kriegslustig <github@ls7.ch>
2015-09-07 18:02:05 +0200

    Version bump

--- Package.js
+++ Package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'halunka:i18n',
-  version: '0.0.6',
+  version: '0.0.7',
   summary: 'Lightweight, reactive, isomorphic, extendable i18n package for meteor using MongoDB',
   git: 'https://github.com/halunka/meteor-i18n'
 })
